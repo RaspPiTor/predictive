2935e54788e9fa5347ae1c2e58989817482c80e3
Alex Williams <hughaw64@gmail.com>
2019-01-28 20:16:20 +0000

    Improved ML
    Added evaluation of current NN

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ impl ML {
         new.randomise();
         new
     }
-    pub fn randomise(&mut self) {
+    fn randomise(&mut self) {
         let mut new_nn: Vec<Vec<f32>> = Vec::with_capacity(self.output_size);
         let mut i: usize = 0;
         while { i < self.output_size } {
@@ -34,7 +34,7 @@ impl ML {
         }
         self.nn = new_nn;
     }
-    pub fn predict(&self, input: Vec<f32>) -> Vec<f32> {
+    pub fn predict(&self, input: &Vec<f32>) -> Vec<f32> {
         let mut output: Vec<f32> = Vec::with_capacity(self.output_size);
         let mut i: usize = 0;
         while { i < self.output_size } {
@@ -48,12 +48,53 @@ impl ML {
                     );
                     ;
                 }
+                x += 1;
             }
+            i += 1;
+            output.push(unsafe { std::intrinsics::fdiv_fast(fast_math::atan(total), 90.0) });
         }
         return output;
     }
+    fn small_change(&mut self) {
+        let mut i: usize = 0;
+        while { i < self.output_size } {
+            let mut x: usize = 0;
+            while { x < self.input_size } {
+                unsafe {
+                    self.nn[i][x] = std::intrinsics::fadd_fast(
+                        self.nn[i][x],
+                        self.rng.gen_range(0.001, -0.001),
+                    );
+                }
+                x += 1;
+            }
+            i += 1;
+        }
+    }
+    pub fn evaluate(&self, training_data: Vec<Vec<Vec<f32>>>) {
+        let mut total_error: f32 = 0.0;
+        let mut row: usize = 0;
+        while { row < training_data.len() } {
+            let predicted: Vec<f32> = self.predict(&training_data[row][0]);
+            let mut i: usize = 0;
+            while { i < self.output_size } {
+                unsafe {
+                    total_error = std::intrinsics::fadd_fast(
+                        total_error,
+                        std::intrinsics::fdiv_fast(predicted[i], training_data[row][1][i]).abs(),
+                    );
+                        ;
+                }
+                i += 1;
+            }
+            row += 1;
+        }
+        total_error;
+    }
+    pub fn train(&mut self, training_data: Vec<Vec<Vec<f32>>>) {}
 }
 fn main() {
-    let mut TheMachine: ML = ML::new(20, 7);
-    println!("{:?}", TheMachine.nn);
+    let mut the_machine: ML = ML::new(4, 2);
+    println!("{:?}", the_machine.nn);
+    println!("{:?}", the_machine.predict(&vec![1.0, 2.0, 3.0, 4.0]));
 }
