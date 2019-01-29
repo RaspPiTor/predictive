6beb4fe1a8ff9eb447cfa3a64de13114223fa12c
Alex Williams <hughaw64@gmail.com>
2019-01-29 19:48:02 +0000

    Added optimise_current This function uses current NN, and tries to find a better one via local optimisation

--- src/main.rs
+++ src/main.rs
@@ -51,25 +51,11 @@ impl ML {
                 x += 1;
             }
             i += 1;
-            output.push(unsafe { std::intrinsics::fdiv_fast(fast_math::atan(total), 90.0) });
+            output.push(unsafe {
+                std::intrinsics::fdiv_fast(fast_math::atan(total), std::f32::consts::PI)
+            });
         }
         return output;
-    }
-    fn small_change(&mut self) {
-        let mut i: usize = 0;
-        while { i < self.output_size } {
-            let mut x: usize = 0;
-            while { x < self.input_size } {
-                unsafe {
-                    self.nn[i][x] = std::intrinsics::fadd_fast(
-                        self.nn[i][x],
-                        self.rng.gen_range(-0.001, 0.001),
-                    );
-                }
-                x += 1;
-            }
-            i += 1;
-        }
     }
     pub fn evaluate(&self, training_data: &Vec<Vec<Vec<f32>>>) -> f32 {
         let mut total_error: f32 = 0.0;
@@ -81,8 +67,7 @@ impl ML {
                 unsafe {
                     total_error = std::intrinsics::fadd_fast(
                         total_error,
-                        std::intrinsics::fsub_fast(predicted[i], training_data[row][1][i])
-                            .abs()
+                        std::intrinsics::fsub_fast(predicted[i], training_data[row][1][i]).abs(),
                     );
                 }
                 i += 1;
@@ -93,7 +78,53 @@ impl ML {
             std::intrinsics::fdiv_fast(total_error, (training_data.len() * self.output_size) as f32)
         }
     }
-    pub fn train(&mut self, training_data: Vec<Vec<Vec<f32>>>) {}
+    pub fn optimise_current(
+        &mut self,
+        training_data: &Vec<Vec<Vec<f32>>>,
+        rounds: u32,
+        variations: u32,
+    ) {
+        let mut previous_score: f32 = self.evaluate(&training_data);
+        for attempt in 0..rounds {
+            let mut best_change: Vec<f32> = Vec::with_capacity(3);
+            let mut new_score: f32 = previous_score + 1.0;
+            let mut i: usize = 0;
+            while { i < self.output_size } {
+                let mut x: usize = 0;
+                while { x < self.input_size } {
+                    for _ in 0..variations {
+                        let change: f32 = self.rng.gen_range(-0.0001, 0.0001);
+                        self.nn[i][x] =
+                            unsafe { std::intrinsics::fadd_fast(self.nn[i][x], change) };
+                        let current_score: f32 = self.evaluate(&training_data);
+                        if { current_score < new_score } {
+                            new_score = current_score;
+                            best_change = vec![i as f32, x as f32, change];
+                        }
+                        self.nn[i][x] =
+                            unsafe { std::intrinsics::fsub_fast(self.nn[i][x], change) };
+                    }
+                    x += 1;
+                }
+                i += 1;
+            }
+            if { new_score < previous_score } {
+                println!("Attempt {:?}, new score: {:?}", attempt, new_score);
+                self.nn[best_change[0] as usize][best_change[1] as usize] = unsafe {
+                    std::intrinsics::fadd_fast(
+                        self.nn[best_change[0] as usize][best_change[1] as usize],
+                        best_change[2],
+                    )
+                };
+                previous_score = new_score;
+            } else {
+                return;
+            }
+        }
+    }
+    pub fn train(&mut self, training_data: &Vec<Vec<Vec<f32>>>) {
+        self.optimise_current(&training_data, 10, 10);
+    }
 }
 fn main() {
     let mut the_machine: ML = ML::new(4, 2);
@@ -101,6 +132,17 @@ fn main() {
     println!("{:?}", the_machine.predict(&vec![1.0, 2.0, 3.0, 4.0]));
     println!(
         "{:?}",
-        the_machine.evaluate(&vec![vec![vec![1.0, 2.0, 3.0, 4.0], vec![0.015, 0.015]]])
+        the_machine.evaluate(&vec![vec![vec![1.0, 2.0, 3.0, 4.0], vec![0.0, 1.0]]])
+    );
+    the_machine.optimise_current(
+        &vec![vec![vec![1.0, 2.0, 3.0, 4.0], vec![0.0, 1.0]]],
+        1000,
+        100000,
+    );
+    println!("{:?}", the_machine.nn);
+    println!("{:?}", the_machine.predict(&vec![1.0, 2.0, 3.0, 4.0]));
+    println!(
+        "{:?}",
+        the_machine.evaluate(&vec![vec![vec![1.0, 2.0, 3.0, 4.0], vec![0.0, 1.0]]])
     );
 }
