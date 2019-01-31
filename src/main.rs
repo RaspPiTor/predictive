f5f21b35973f7714d8a95f9342a6eeee81f26c7a
Alex Williams <hughaw64@gmail.com>
2019-01-31 21:00:36 +0000

    Changed vector used

--- src/main.rs
+++ src/main.rs
@@ -166,7 +166,6 @@ fn main() {
     let mut the_machine: ML = ML::new(4, 5, 5, 5);
     the_machine.train(
         &vec![
-            vec![vec![1.0, 2.0, 3.0, 4.0], vec![0.0, 0.25, 0.5, 0.75, 1.0]],
             vec![vec![4.0, 3.0, 2.0, 1.0], vec![1.0, 0.75, 0.5, 0.25, 0.0]],
         ],
         10000,
